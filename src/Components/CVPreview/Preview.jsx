86b699e3a4284cc3400ec720b1901f7f653111d6
AHpxMac <ahpx@yandex.com>
2024-10-12 12:58:23 +0800

    Parameterize

--- src/Components/CVPreview/Preview.jsx
+++ src/Components/CVPreview/Preview.jsx
@@ -1,39 +1,72 @@
-import Card from "../Universal/Card"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons'
-import Experience from "./Experience"
+import Card from "../Universal/Card";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope, faPhone } from "@fortawesome/free-solid-svg-icons";
+import Experience from "./Experience";
 
-
-export default function Preview() {
-    return (<Card style={{
-        // height: '90vh',
-      }}>
-        <div style={{display: 'flex', alignItems: 'center', flexDirection:'column'}}>
-            <h2>Jonathan Joestar</h2>
-            <div>
-                <span style={{marginRight: '10px'}}>
-                    <FontAwesomeIcon icon={faEnvelope} style={{ marginRight: '8px' }} />
-                    ahpx@yandex.com
-                </span>
-                <span>
-                    <FontAwesomeIcon icon={faPhone} style={{ marginRight: '8px' }} />
-                    +86 1145141919
-                </span>
-            </div>
+export default function Preview({
+  name = "Jonathan Joestar",
+  email = "jojo@joestar.com",
+  phone = "+1 1145141919",
+  education = [
+    <Experience
+      startTime="2023/3"
+      endTime="2024/3"
+      title="Oxford University"
+      subtitle="Bachelor Degree of Sociology"
+      key="default1"
+    />,
+    <Experience
+      startTime="2023/3"
+      endTime="2024/3"
+      title="Oxford University"
+      subtitle="Master Degree of Sociology"
+      key="default2"
+    />,
+  ],
+  experience = [
+    <Experience
+      startTime="2021/6"
+      endTime="2022/8"
+      title="Intern"
+      subtitle="Tech Company"
+      description="Worked on developing and maintaining web applications."
+      key="default1"
+    />,
+  ],
+}) {
+  return (
+    <Card>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          flexDirection: "column",
+        }}
+      >
+        <h2>{name}</h2>
+        <div>
+          <span style={{ marginRight: "10px" }}>
+            <FontAwesomeIcon icon={faEnvelope} style={{ marginRight: "8px" }} />
+            {email}
+          </span>
+          <span>
+            <FontAwesomeIcon icon={faPhone} style={{ marginRight: "8px" }} />
+            {phone}
+          </span>
         </div>
-        <hr/>
-        <div style={{
-            display: 'flex',
-            flexDirection: 'column',
-        }}>
-            <h3>Education</h3>
-            <Experience startTime='2023/3' endTime='2024/3' title='Oxford University' subtitle='Bachelor Degree of Sociology'/>
-            <Experience startTime='2023/3' endTime='2024/3' title='Oxford University' subtitle='Master Degree of Sociology'/>
-            <h3>Experience</h3>
-            <Experience startTime='2021/6' endTime='2022/8' title='Intern' subtitle='Tech Company' description='Worked on developing and maintaining web applications.'/>
-            <Experience startTime='2020/5' endTime='2021/5' title='Research Assistant' subtitle='University Lab' description='Assisted in research projects and data analysis.'/>
-            <Experience startTime='2019/7' endTime='2020/4' title='Volunteer' subtitle='Non-Profit Organization' description='Organized community events and managed social media accounts.'/>
-        </div>
-      </Card>
-    )
+      </div>
+      <hr />
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        <h3>Education</h3>
+        {education}
+        <h3>Experience</h3>
+        {experience}
+      </div>
+    </Card>
+  );
 }
